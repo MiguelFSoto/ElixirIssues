7fba031c5d845057b51ea46b73a6d7334b761a8b
Miguel Soto <miguel@soto.net.co>
2023-09-08 22:30:28 +0000

    Add CLI handling

---
+++ lib/issues/cli.ex
@@ -0,0 +1,30 @@
+defmodule Issues.CLI do
+    @moduledoc """
+    Handling of command line arguments for the app
+    """
+    
+    def run(argv), do: parse(argv)
+    @doc """
+    argv can contain -h/--help for :help
+    Otherwise, it contains github user, repo and
+    number of issues to grab
+    """
+    def parse(argv) do
+        argvParse = OptionParser.parse(argv, switches: [help: :boolean],
+                                       aliases: [h: :help])
+        defaultIssues = 5
+        case argvParse do
+            #Checks if help was called
+            {[help: true], _, _} 
+                -> :help
+            #Gets passed arguments
+            {_, [user, repo, count], _}
+                -> {user, repo, count}
+            #Gets arguments and defaults the number of issues
+            {_, [user, repo], _}
+                -> {user, repo, defaultIssues}
+            #Defaults to help
+            _ -> :help
+        end
+    end
+end
